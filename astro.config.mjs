6d65938dd95a4c34929c7f59044eac86aef3ca66
RubyPG <ruben.pezuelagarcia@gmail.com>
2026-01-23 23:00:16 +0100

    Prueba de que no se rompe"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 ﻿import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+const isUserSite = repo.endsWith(".github.io");
+const base =
+  process.env.ASTRO_BASE ??
+  (isGithubActions ? (isUserSite ? "/" : `/${repo}`) : "/");
+
 export default defineConfig({
   integrations: [tailwind()],
+  base,
 });
